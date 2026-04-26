fe556388ebf06f515c70b4ff756bf07f27e19a32
kjh3165 <jhwk109@gmail.com>
2026-04-26 20:36:57 +0900

    t6

--- src/main/java/com/ll/domain/testPost/testPost/service/TestFacadePostService.java
+++ src/main/java/com/ll/domain/testPost/testPost/service/TestFacadePostService.java
@@ -1,10 +1,11 @@
 package com.ll.domain.testPost.testPost.service;
 
 import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.framework.ioc.ApplicationContext;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class TestFacadePostService {
-    private final TestPostService testPostService;
-    private final TestPostRepository testPostRepository;
+    private final TestPostService testPostService = ApplicationContext.testPostService;
+    private final TestPostRepository testPostRepository = ApplicationContext.testPostRepository;
 }
--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,21 +1,25 @@
 package com.ll.framework.ioc;
 
 import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestFacadePostService;
 import com.ll.domain.testPost.testPost.service.TestPostService;
 
 public class ApplicationContext {
     public static TestPostRepository testPostRepository;
     public static TestPostService testPostService;
+    public static TestFacadePostService testFacadePostService;
 
     public ApplicationContext() {
         testPostRepository = new TestPostRepository();
         testPostService = new TestPostService();
+        testFacadePostService = new TestFacadePostService();
     }
 
     public <T> T genBean(String beanName) {
         return (T) switch(beanName){
             case("testPostService") -> testPostService;
             case("testPostRepository") -> testPostRepository;
+            case("testFacadePostService") -> testFacadePostService;
             default -> null;
         };
     }
